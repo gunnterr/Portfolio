d1c1cb2e060449af913d0e0d942c77054456b63d
gunnterr <gun_kgmb@hotmail.com>
2024-04-24 01:44:18 +0700

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    open: true,
+    compress: true,
+    port: 9000,
+  },
+};
